52d5965978bbae342bc2acd4bb47784e23d866ef
fratorgano <francesco.torgano@outlook.it>
2020-12-22 20:10:33 +0100

    day22 optimized

--- day22/src/lib.rs
+++ day22/src/lib.rs
@@ -1,7 +1,8 @@
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
+use std::collections::HashSet;
 
-fn parse_hands(input:&String) -> (Vec<usize>,Vec<usize>){
+fn parse_hands(input:&String) -> (Vec<u8>,Vec<u8>){
     let mut hands:Vec<&str> = input.split("Player 2:\n").collect();
     hands[0] = hands[0].strip_prefix("Player 1:\n").unwrap().trim();
     hands[1] = hands[1].trim();
@@ -15,7 +16,6 @@ fn parse_hands(input:&String) -> (Vec<usize>,Vec<usize>){
     }
     p1.reverse();
     p2.reverse();
-    //println!("{:?}\n{:?}",p1,p2);
     (p1,p2)
 }
 
@@ -31,16 +31,14 @@ pub fn space_card_combat(input:&String) -> usize{
         match card_p1>card_p2 {
             true => {
                 //println!("P1 wins");
-                let mut add = vec!(card_p2,card_p1);
-                add.append(&mut p1);
-                p1 = add;
+                p1.insert(0,card_p2);
+                p1.insert(1,card_p1);
                 //println!("{:?}",p1);
             },
             false => {
                 //println!("P2 wins");
-                let mut add = vec!(card_p1,card_p2);
-                add.append(&mut p2);
-                p2 = add;
+                p2.insert(0,card_p1);
+                p2.insert(1,card_p2);
                 //println!("{:?}",p2);
             }
         }
@@ -50,14 +48,14 @@ pub fn space_card_combat(input:&String) -> usize{
     if p1.is_empty() {winner = p2} else {winner = p1}
     //println!("{:?}",winner);
     for (i,v) in winner.iter().enumerate(){
-        score += v*(i+1);
+        score += *v as usize*(i+1);
     }
     score
 }
 
 pub fn recursive_space_card_combat(input:&String) -> usize{
     let (mut p1,mut p2) = parse_hands(input);
-    let mut hist = vec![];
+    let mut hist = HashSet::new();
     while !p1.is_empty() && !p2.is_empty(){
         let winner = recursive_round(&mut p1, &mut p2,&mut hist);
         let card_p1 = winner.0;
@@ -65,16 +63,20 @@ pub fn recursive_space_card_combat(input:&String) -> usize{
         match winner.2 {
             1 => {
                 //println!("P1 wins");
-                let mut add = vec!(card_p2,card_p1);
+                /* let mut add = vec!(card_p2,card_p1);
                 add.append(&mut p1);
-                p1 = add;
+                p1 = add; */
+                p1.insert(0,card_p2);
+                p1.insert(1,card_p1);
                 //println!("{:?}",p1);
             },
             2 => {
                 //println!("P2 wins");
-                let mut add = vec!(card_p1,card_p2);
+                /* let mut add = vec!(card_p1,card_p2);
                 add.append(&mut p2);
-                p2 = add;
+                p2 = add; */
+                p2.insert(0,card_p1);
+                p2.insert(1,card_p2);
                 //println!("{:?}",p2);
             },
             3 => {
@@ -88,15 +90,15 @@ pub fn recursive_space_card_combat(input:&String) -> usize{
     let mut score = 0;
     if p1.is_empty() {winner = p2} else {winner = p1}
     for (i,v) in winner.iter().enumerate(){
-        score += v*(i+1);
+        score += *v as usize *(i+1);
     }
     score
 }
 
 
-fn recursive_round(p1:&mut Vec<usize>, p2:&mut Vec<usize>, hist:&mut Vec<u64>) -> (usize,usize,usize){
+fn recursive_round(p1:&mut Vec<u8>, p2:&mut Vec<u8>, hist:&mut HashSet<u64>) -> (u8,u8,u8){
     let h64 = hash((&p1,&p2));
-    if hist.contains(&h64){ return (1,1,3) } else { hist.push(h64) };
+    if hist.contains(&h64){ return (1,1,3) } else { hist.insert(h64) };
 
     //println!("ROUND: {}",hist.len());
     //println!("Deck 1: {:?}",p1);
@@ -105,15 +107,23 @@ fn recursive_round(p1:&mut Vec<usize>, p2:&mut Vec<usize>, hist:&mut Vec<u64>) -
     let card_p1 = p1.pop().unwrap();
     let card_p2 = p2.pop().unwrap();
 
-    if p1.len()>=card_p1 && p2.len()>=card_p2{
-        let mut hist2 = vec![];
-        let mut rec_p1:Vec<usize> = p1[p1.len()-card_p1..p1.len()].to_vec();
-        let mut rec_p2:Vec<usize> = p2[p2.len()-card_p2..p2.len()].to_vec();
-        let mut winner = (0,0,0);
+    if p1.len()>=card_p1 as usize && p2.len()>=card_p2 as usize{
+
+        let mut rec_p1:Vec<u8> = p1[p1.len()-card_p1 as usize..p1.len()].to_vec();
+        let mut rec_p2:Vec<u8> = p2[p2.len()-card_p2 as usize..p2.len()].to_vec();
+
+        //If P1 has the highest card he can't lose subgames
+        if rec_p1.iter().max()>rec_p2.iter().max(){
+            return (card_p1,card_p2,1)
+        }
+        
+        
         //println!("Starting inner game\n");
         //println!("Inner Deck 1: {:?} ({})",rec_p1, !rec_p1.is_empty());
         //println!("Inner Deck 2: {:?} ({})\n",rec_p2, !rec_p2.is_empty());
         //println!("Ciclo? {}",!rec_p1.is_empty() && !rec_p2.is_empty());
+        let mut winner = (0,0,0);
+        let mut hist2 = HashSet::new();
         while !rec_p1.is_empty() && !rec_p2.is_empty(){
             winner = recursive_round(&mut rec_p1, &mut rec_p2, &mut hist2);
             let card_p1 = winner.0;
@@ -121,16 +131,20 @@ fn recursive_round(p1:&mut Vec<usize>, p2:&mut Vec<usize>, hist:&mut Vec<u64>) -
             match winner.2 {
                 1 => {
                     //println!("P1 wins");
-                    let mut add = vec!(card_p2,card_p1);
+                    /* let mut add = vec!(card_p2,card_p1);
                     add.append(&mut rec_p1);
-                    rec_p1 = add;
+                    rec_p1 = add; */
+                    rec_p1.insert(0,card_p2);
+                    rec_p1.insert(1,card_p1);
                     //println!("{:?}",p1);
                 },
                 2 => {
                     //println!("P2 wins");
-                    let mut add = vec!(card_p1,card_p2);
+                    /* let mut add = vec!(card_p1,card_p2);
                     add.append(&mut rec_p2);
-                    rec_p2 = add;
+                    rec_p2 = add; */
+                    rec_p2.insert(0,card_p1);
+                    rec_p2.insert(1,card_p2);
                     //println!("{:?}",p2);
                 },
                 3 => {
