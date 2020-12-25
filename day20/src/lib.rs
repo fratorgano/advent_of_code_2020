fa518dfa138bcad1a330611e5395b4fe642db35b
fratorgano <francesco.torgano@outlook.it>
2020-12-25 10:44:03 +0100

    day20 fixed

--- day20/src/lib.rs
+++ day20/src/lib.rs
@@ -1,12 +1,392 @@
-//Today was way too hard and I couldn't solve it in the time that I had. I will probably come back to it eventually.
+use std::collections::{
+    HashSet,
+    HashMap,
+};
+use std::cmp;
 
+const PATTERN: &'static str = 
+"                  # 
+#    ##    ##    ###
+ #  #  #  #  #  #   ";
 
+#[derive(Debug, PartialEq, Eq, Hash, Clone)]
+struct Tile {
+    id: usize,
+    content: Vec<Vec<bool>>,
+    borders: Vec<Vec<bool>>,
+}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Tile {
+    fn from_raw(raw: &str) -> Self {
+        let mut iterable = raw
+            .trim()
+            .split('\n');
 
-    #[test]
-    fn day20_test() {
+        let id = if let Some(line) = iterable.next() {
+            let mut parts = line.trim().split_whitespace();
+            parts.next();
+            if let Some(raw_number) = parts.next() {
+                raw_number[..raw_number.len() - 1].parse::<usize>().unwrap()
+            } else {
+                panic!()
+            }
+        } else {
+            panic!()
+        };
+
+        let content: Vec<_> = iterable
+            .map(|line| line.trim().chars().map(|c| c == '#').collect::<Vec<_>>())
+            .collect();
+
+        return Self::new(id, content);
     }
+
+    fn new(id: usize, content: Vec<Vec<bool>>) -> Self {
+        let top = content[0].clone();
+        let right: Vec<_> = content.iter().map(|row| row[row.len() - 1]).collect();
+        let bottom = content[content.len() - 1].clone();
+        let left: Vec<_> = content.iter().map(|row| row[0]).collect();
+        
+        return Tile { id: id, content: content, borders: vec![top, right, bottom, left]};
+    }
+
+    fn matched_borders(&self, another: &Tile) -> Option<(usize, usize, bool)> {
+        for i in 0..4 {
+            for j in 0..4 {
+                if self.borders[i] == another.borders[j] {
+                    return Some((i, j, false));
+                } 
+                if self.borders[i] == another.borders[j].iter().cloned().rev().collect::<Vec<_>>() {
+                    return Some((i, j, true));
+                } 
+            }
+        }
+        return None;
+    }
+
+    fn matches_with(&self, another: &Tile) -> bool {
+        self.matched_borders(another).is_some()
+    }
+
+    fn rotate(&self) -> Self {
+        let content = rotate_content(&self.content);
+        return Self::new(self.id, content);
+     }
+
+    fn swap(&self) -> Self {
+        let content = swap_content(&self.content);
+        return Self::new(self.id, content);
+    }
+
+    fn content_without_border(&self) -> Vec<Vec<bool>> {
+        let mut ans = Vec::new();
+        for i in 1..self.content.len() - 1 {
+            let mut row = Vec::new();
+            for j in 1..self.content[0].len() - 1 {
+                row.push(self.content[i][j]);
+            }
+            ans.push(row);
+        }
+        return ans;
+    }
+}
+
+pub fn multiply_corner_ids(input:&String) -> usize{
+    let tiles: Vec<_> = input
+        .trim()
+        .split("\n\n")
+        .map(|raw| Tile::from_raw(raw))
+        .collect();
+    
+    
+    let mut matches = HashMap::new(); 
+    for i in 0..tiles.len() {
+        let tile_i = &tiles[i];
+        for j in (i + 1)..tiles.len() {
+            let tile_j = &tiles[j];
+            if tile_i.matches_with(tile_j) {
+                matches.entry(tile_i).or_insert_with(HashSet::new).insert(tile_j);
+                matches.entry(tile_j).or_insert_with(HashSet::new).insert(tile_i);
+            }
+        }
+    }
+    
+    let ans: usize = matches
+        .iter()
+        .filter(|(_, v)| v.len() == 2)
+        .map(|(k, _)| k.id)
+        .product();
+
+    ans
+}
+
+fn rotate_content(content: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
+    let mut rotated_content = Vec::new();
+    for j in (0..content[0].len()).rev() {
+        let mut row = Vec::new();
+        for i in 0..content.len() {
+            row.push(content[i][j].clone());
+        }
+        rotated_content.push(row);
+    }
+    return rotated_content;
+}
+
+fn swap_content(content: &Vec<Vec<bool>>) -> Vec<Vec<bool>> {
+    let mut swapped_content = Vec::new();
+
+    for row in content.iter() {
+        let tmp: Vec<_> = row.iter().cloned().rev().collect();
+        swapped_content.push(tmp);
+    }
+    return swapped_content;
+}
+
+fn compose_image(matches: &HashMap<&Tile, HashSet<&Tile>>) -> Vec<Vec<bool>> {
+    let structure = compose_board_structure(matches);
+    let board = orient_tiles(&structure);
+
+    let contents = board
+        .into_iter()
+        .fold(Vec::new(), |mut full, row| {
+            let n_r = row[0].content_without_border().len();
+            let contents = row
+                .into_iter()
+                .fold(vec![Vec::new(); n_r], |mut full_row, tile| {
+                    let content = tile.content_without_border();
+
+                    for i in 0..content.len() {
+                        full_row[i].extend(content[i].clone());
+                    } 
+                    return full_row;
+                });
+
+            for c in contents {
+                full.push(c);
+            }
+
+            return full;
+            
+        });
+
+    return contents;
+}
+
+fn orient_tiles(structure: &Vec<Vec<&Tile>>) -> Vec<Vec<Tile>> {
+    let mut first_tile = structure[0][0].clone();
+    if let Some(ans) = compose_board(&structure, &first_tile) {
+        return ans;
+    }
+    for _ in 0..2 {
+        first_tile = first_tile.swap();
+        for _ in 0..4 {
+            first_tile = first_tile.rotate();
+            if let Some(ans) = compose_board(&structure, &first_tile) {
+                return ans;
+            }
+        }
+    }
+    panic!();
+}
+
+fn compose_board<'a>(structure: &Vec<Vec<&Tile>>, first_tile: &Tile) -> Option<Vec<Vec<Tile>>> {
+    let mut board: Vec<Vec<Tile>> = Vec::new();
+    for i in 0..structure.len() {
+        let mut row = Vec::new();
+        for j in 0..structure[0].len() {
+            if i == 0 && j == 0 {
+                row.push(first_tile.clone());
+                continue;
+            }
+            let mut tile = structure[i][j].clone();
+            if i > 0 {
+                let mut matched = false;
+                for _ in 0..2 {
+                    tile = tile.swap();
+                    for _ in 0..4 {
+                        tile = tile.rotate();
+                        let (first_rot, second_rot, swap) = board[i - 1][j].matched_borders(&tile).unwrap();
+                        if first_rot == 2 && second_rot == 0 && !swap {
+                            matched = true;
+                            break;
+                        }
+                    }
+                    if matched {
+                        break;
+                    }
+                }
+                if !matched {
+                    return None;
+                }
+            }
+            if j > 0 {
+                let mut matched = false;
+                for _ in 0..2 {
+                    tile = tile.swap();
+                    for _ in 0..4 {
+                        tile = tile.rotate();
+                        let (first_rot, second_rot, swap) = row[j - 1].matched_borders(&tile).unwrap();
+                        if first_rot == 1 && second_rot == 3 && !swap {
+                            matched = true;
+                            break;
+                        }
+                    }
+                    if matched {
+                        break;
+                    }
+                }
+                if !matched {
+                    return None;
+                }
+            }
+            row.push(tile);
+        }
+        board.push(row);
+    }
+    return Some(board);
+}
+
+fn compose_board_structure<'a>(matches: &HashMap<&'a Tile, HashSet<&'a Tile>>) -> Vec<Vec<&'a Tile>> {
+    let mut chosen = HashSet::new();
+
+    let mut board = Vec::new();
+
+    let first = matches.iter().filter(|(_, v)| v.len() == 2).map(|(k, _)| k).next().unwrap();
+
+    let mut row = Vec::new();
+    let mut current = first;
+    while let Some(next) = matches[current]
+        .iter()
+        .filter(|&tile| !chosen.contains(tile) && matches[tile].len() == 3)
+        .next() 
+    {
+        row.push(*current);
+        chosen.insert(current);
+        current = next;
+    }
+    row.push(*current);
+    chosen.insert(current);
+
+    if let Some(next) = matches[current]
+        .iter()
+        .filter(|&tile| !chosen.contains(tile) && matches[tile].len() == 2)
+        .next()
+    {
+        row.push(*next);
+    }
+
+    let m = row.len();
+    board.push(row);
+
+    while let Some(first) = matches[board[board.len() - 1][0]]
+        .iter()
+        .filter(|&tile| !chosen.contains(tile) && matches[tile].len() < 4)
+        .next() 
+    {
+        let mut row = Vec::new();
+        let mut current = first;
+        let mut i = 0;
+        while let Some(next) = matches[current]
+            .iter()
+            .filter(|&tile| !chosen.contains(tile) && i < m - 1 && matches[tile].contains(board[board.len() - 1][i + 1]))
+            .next() 
+        {
+            row.push(*current);
+            chosen.insert(current);
+            i += 1;
+            current = next;
+        }
+        row.push(*current);
+        chosen.insert(current);
+
+        board.push(row);
+
+    }
+   
+    return board;
+}
+
+fn find_pattern(image: &Vec<Vec<bool>>, pattern: &Vec<Vec<bool>>) -> usize {
+    let mut image = image.clone();
+    let mut any_matched = false;
+    for i in 0..(image.len() - pattern.len()) {
+        for j in 0..(image[0].len() - pattern[0].len()) {
+
+            let mut matched = true;
+            for k in 0..pattern.len() {
+                for l in 0..pattern[0].len() {
+                    if !pattern[k][l] {
+                        continue;
+                    }
+                    if !image[i + k][j + l] {
+                        matched = false;
+                        break;
+                    }
+                }
+                if !matched {
+                    break;
+                }
+            }
+            if matched {
+                any_matched = true;
+                for k in 0..pattern.len() {
+                    for l in 0..pattern[0].len() {
+                        if !pattern[k][l] {
+                            continue;
+                        }
+                        if image[i + k][j + l] {
+                           image[i + k][j + l] = false; 
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if !any_matched {
+        return 0;
+    }
+    return image
+        .into_iter()
+        .map(|row| row.into_iter().filter(|cell| *cell).count())
+        .sum();
+}
+
+pub fn count_sharp_no_sea_monster(input:&String) -> usize{
+    let tiles: Vec<_> = input
+        .trim()
+        .split("\n\n")
+        .map(|raw| Tile::from_raw(raw))
+        .collect();
+    
+    let mut matches = HashMap::new(); 
+    for i in 0..tiles.len() {
+        let tile_i = &tiles[i];
+        for j in (i + 1)..tiles.len() {
+            let tile_j = &tiles[j];
+            if tile_i.matches_with(tile_j) {
+                matches.entry(tile_i).or_insert_with(HashSet::new).insert(tile_j);
+                matches.entry(tile_j).or_insert_with(HashSet::new).insert(tile_i);
+            }
+        }
+    }
+    
+    let image = compose_image(&matches);
+    
+    let mut pattern: Vec<_> = PATTERN
+        .split('\n')
+        .map(|row| row.chars().map(|c| c == '#').collect::<Vec<_>>())
+        .collect();
+
+    let mut ans = 0;
+    for _ in 0..2 {
+        pattern = swap_content(&pattern);
+        for _ in 0..4 {
+            pattern = rotate_content(&pattern);
+            
+            let tmp = find_pattern(&image, &pattern);
+            ans = cmp::max(ans, tmp);
+        }
+    }
+
+    ans
 }
--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,4 +1,5 @@
-//Today was way too hard and I couldn't solve it in the time that I had. I will probably come back to it eventually.
+//20/12/2020: Today was way too hard and I couldn't solve it in the time that I had. I will probably come back to it eventually.
+//25/12/2020: I did it. I admittedly looked up some other solutions and took inspiration from it.
 
 use std::env;
 use std::fs;
@@ -7,16 +8,16 @@ use std::time::Instant;
 fn main() {
     let input = read_file_args();
 
-    /* let start = Instant::now();
-    let res1 = day19::count_valid(&rules, &input);
-    println!("Finished after {:?}", start.elapsed()); */
+    let start = Instant::now();
+    let res1 = day20::multiply_corner_ids(&input);
+    println!("Finished after {:?}", start.elapsed());
 
-    /* let start = Instant::now();
-    let res2 = day19::count_valid_2(&rules, &input);
-    println!("Finished after {:?}", start.elapsed()); */
+    let start = Instant::now();
+    let res2 = day20::count_sharp_no_sea_monster(&input);
+    println!("Finished after {:?}", start.elapsed());
 
-    println!("Part 1 result: {}",1);
-    println!("Part 2 result: {}",2);
+    println!("Part 1 result: {}",res1);
+    println!("Part 2 result: {}",res2);
 }
 
 fn read_file_args() -> String {
